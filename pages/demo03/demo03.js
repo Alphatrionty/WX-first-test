8041d351a1d173b329305538800d5ef8dfcfcb65
Alphatrionty <1379844203@qq.com>
2020-05-02 19:03:09 +0800

    test

---
+++ pages/demo03/demo03.js
@@ -0,0 +1,29 @@
+// pages/demo03/demo03.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg: "hello world",
+    num: 10000,
+    person:{
+      age: 18,
+      height: 1.88
+    },
+    list:[
+      {
+        id:0,
+        name:"猪八戒"
+      },
+      {
+        id:1,
+        name:"天蓬元帅"
+      },
+      {
+        id:2,
+        name:"孙悟空"
+      }
+    ]
+  }
+})
